19d6365abbe8c139505f4184eb7c6df22864c33d
youjin-hub <leeyoujin0704@naver.com>
2026-01-20 21:19:06 +0900

    faet: 회원가입 API Route 구현 및 연결

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,7 +1,14 @@
-﻿export default function SignupPage() {
+﻿'use client';
+
+import { SignupForm } from '../components/SignupForm';
+
+export default function SignupPage() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center p-4">
       <h1>Signup Page</h1>
+      <div className="mt-4 w-full max-w-md">
+        <SignupForm />
+      </div>
     </main>
   );
 }
