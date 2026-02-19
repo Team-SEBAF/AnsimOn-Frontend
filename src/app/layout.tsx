ca96921c4fecc5e66b87666a4be973a7d0513979
youjin-hub <leeyoujin0704@naver.com>
2026-02-19 11:39:12 +0900

    feat: tanstack query 설치 및 provider 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/styles/index.css';
 import { AuthProvider } from '@/components/providers/AuthProvider';
+import { QueryProvider } from '@/components/providers/QueryClientProvider';
 
 export const metadata: Metadata = {
   title: 'AnsimOn',
@@ -18,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" className={inter.variable}>
       <body>
-        <AuthProvider>{children}</AuthProvider>
+        <QueryProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </QueryProvider>
       </body>
     </html>
   );
